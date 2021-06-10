acdef8fde85a477f9767ce89d24644076203293c
dennis9352 <85466642+dennis9352@users.noreply.github.com>
2021-06-10 22:51:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,15 @@
 from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-
 app = Flask(__name__)
-
 from pymongo import MongoClient
 
 client = MongoClient('mongodb://3.37.86.16', 27017, username="test", password="test")
 db = client.dbsparta
-# JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
-# 이 문자열은 서버만 알고있기 때문에, 내 서버에서만 토큰을 인코딩(=만들기)/디코딩(=풀기) 할 수 있습니다.
 SECRET_KEY = 'SPARTA'
 
-# JWT 패키지를 사용합니다. (설치해야할 패키지 이름: PyJWT)
 import jwt
-
-# 토큰에 만료시간을 줘야하기 때문에, datetime 모듈도 사용합니다.
 import datetime
-
-# 회원가입 시엔, 비밀번호를 암호화하여 DB에 저장해두는 게 좋습니다.
-# 그렇지 않으면, 개발자(=나)가 회원들의 비밀번호를 볼 수 있으니까요.^^;
 import hashlib
 
-
-#################################
-##  HTML을 주는 부분             ##
-#################################
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
@@ -32,15 +18,9 @@ def home():
         user_info = db.user.find_one({"id": payload['id']})
         return render_template('main.html', name=user_info["name"])
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return render_template('signup.html', msg="로그인 시간이 만료되었습니다.")
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login"))
-
-
-# @app.route('/login')
-# def login():
-#     msg = request.args.get("msg")
-#     return render_template('login.html')
+        return render_template('signup.html')
 
 
 @app.route('/signup')
@@ -66,18 +46,6 @@ def write():
     return render_template('write.html', name=user_info['name'][1:])
 
 
-@app.route('/edit')
-def edit():
-    return render_template('edit.html')
-
-
-#################################
-##  로그인을 위한 API            ##
-#################################
-
-# [회원가입 API]
-# id, pw, nickname을 받아서, mongoDB에 저장합니다.
-# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
 @app.route('/api/signup/ok', methods=['POST'])
 def api_register():
     id_receive = request.form['id_give']
@@ -90,9 +58,6 @@ def api_register():
 
     return jsonify({'result': 'success'})
 
-
-# [로그인 API]
-# id, pw를 받아서 맞춰보고, 토큰을 만들어 발급합니다.
 @app.route('/api/login', methods=['POST'])
 def api_login():
     id_receive = request.form['id_give']
@@ -108,7 +73,7 @@ def api_login():
             'id': id_receive,
             'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
         return jsonify({'result': 'success', 'token': token})
 
@@ -160,17 +125,6 @@ def listing():
         diary["_id"] = str(diary["_id"])
 
     return jsonify({'user_diaries': diaries})
-
-
-# @app.route('/api/edit', methods=['GET'])
-# def popup():
-#     token_receive = request.cookies.get('mytoken')
-#     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#     user_info = db.user.find_one({'id': payload['id']})
-#     diary = db.diaries.find_one({'id': user_info['id']})
-#     diary["_id"] = str(diary["_id"])
-#
-#     return jsonify({'diary': diary})
 
 @app.route('/api/edit', methods=['POST'])
 def editSave():
